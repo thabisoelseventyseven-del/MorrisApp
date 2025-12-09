48aade6bc569a5cf271691ac4199b080e9c87c47
thabisoelseventyseven-del <thabisoelseventyseven@gmail.com>
2025-12-09 10:01:51 +0200

    updated the buttons

--- WebApplication1/wwwroot/app.js
+++ WebApplication1/wwwroot/app.js
@@ -1,6 +1,4 @@
-﻿// Simple simulation of login and register for now
-
-function registerUser() {
+﻿function registerUser() {
     const email = document.getElementById('regEmail').value;
     const password = document.getElementById('regPassword').value;
 
@@ -9,9 +7,7 @@ function registerUser() {
         return;
     }
 
-    // For now, just alert
     alert(`Registered successfully!\nEmail: ${email}\nPassword: ${password}`);
-    // You can later integrate Firebase Auth here
     document.getElementById('regEmail').value = '';
     document.getElementById('regPassword').value = '';
 }
@@ -25,9 +21,7 @@ function loginUser() {
         return;
     }
 
-    // For now, just alert
     alert(`Logged in successfully!\nEmail: ${email}`);
-    // You can later redirect to dashboard.html
     document.getElementById('loginEmail').value = '';
     document.getElementById('loginPassword').value = '';
 }
